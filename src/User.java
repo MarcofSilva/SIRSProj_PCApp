879dec1a3d14d45fbf5cd394e85489dc08cdc04d
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-11-27 19:55:13 +0000

    cenas

--- src/User.java
+++ src/User.java
@@ -1,6 +1,6 @@
 public class User {
     private String _username;
-    private String _password; //TODO password shouldn't be stores this way
+    private String _password; //TODO password shouldn't be stored this way
 
     public User(String username, String password){
         _username = username;
