5730f34982cc06038dc030ab025a3714a51964a5
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-12-11 17:33:17 +0000

    AuthorizationHandler class added

---
+++ src/Main/AuthorizationHandler.java
@@ -0,0 +1,14 @@
+package Main;
+
+
+public class AuthorizationHandler {
+
+    public byte[] addTimestamp(byte[] msg){
+        return msg; //TODO
+    }
+
+    //Returns null if invalid
+    public byte[] validateTimestamp(byte[] msg) {
+        return msg; //TODO
+    }
+}
