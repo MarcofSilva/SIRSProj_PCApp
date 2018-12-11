abd1e344a24a7a308d59903fc193e0a26b18d370
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-12-11 16:58:01 +0000

    MACHandler class todos

--- src/Main/MACHandler.java
+++ src/Main/MACHandler.java
@@ -2,12 +2,19 @@ package Main;
 
 public class MACHandler {
 
-    public byte[] addMAC(byte[] msg){
-        return msg; //TODO
+    public byte[] getMAC(byte[] array){
+        //TODO calculate and return MAC
+        return array; //TODO
     }
 
-    //Returns null if invalid
-    public byte[] validateMAC(byte[] msg) {
-        return msg; //TODO
+    //Returns null if invalid and msg without the mac in the end
+    public byte[] validateMAC(byte[] array) {
+        //TODO
+        //getMAC(com parte inicial da msg que nao contem o mac)
+        /*if(maccalculado != getMAC) {
+            return null;
+        }*/
+        //return parte inicial da msg sem o mac
+        return array; //TODO
     }
 }
