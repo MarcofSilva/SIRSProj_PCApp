39c21582a019346ee15a97ed7bc789bc6ce75d1f
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-12-11 15:41:48 +0000

    MACHandler class added

---
+++ src/Main/MACHandler.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class MACHandler {
+
+    public byte[] addMAC(byte[] msg){
+        return msg; //TODO
+    }
+
+    //Returns null if invalid
+    public byte[] validateMAC(byte[] msg) {
+        return msg; //TODO
+    }
+}
